d10e455c8e146aeedc8ca9a827156f77980e6198
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-08-01 03:06:15 -0700

    launch file made.It will help with jest vs extension

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import AgeCalculator from './js/age-calculator.js';
-//import AgeExpectancy from './js/expectedAge.js';
+import AgeCalculator from './js/calculator.js';
+import AgeExpectancy from './js/expectedAge.js';
 
 
 $(document).ready(function() {
