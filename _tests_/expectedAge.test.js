c158fd322e890c3e8ad9b487c22cf79a22cdf804
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-08-08 00:24:58 -0700

    test 2 and 3 for age-expectancy.test

--- _tests_/expectedAge.test.js
+++ _tests_/expectedAge.test.js
@@ -12,3 +12,26 @@ describe('AgeExpectancy', () => {
         expect(newTest.regionCalculator()).toEqual(73);
     });
 });
+describe('AgeExpectancy', () => {
+    test('should  return average life expectancy based on region', () => {
+        const age = 30;
+        const region = "Africa";
+        const gender = "female";
+        const tobacco = "heavy";
+        const alcohol = "moderate";
+        const newTest = new AgeExpectancy(age, region, gender, tobacco, alcohol);
+        expect(newTest.regionCalculator()).toEqual(64);
+    });
+});
+
+describe('AgeExpectancy', () => {
+    test('should  return average life expectancy based on region', () => {
+        const age = 30;
+        const region = "Eastern Mediterranean";
+        const gender = "female";
+        const tobacco = "heavy";
+        const alcohol = "moderate";
+        const newTest = new AgeExpectancy(age, region, gender, tobacco, alcohol);
+        expect(newTest.regionCalculator()).toEqual(70);
+    });
+});
