28f0e1dc782cd72550ef3d94f5141513339b39c9
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-08-08 00:11:36 -0700

    test for mars and jupiter

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -1,4 +1,4 @@
-//import { TestScheduler } from 'jest';
+import { TestScheduler } from 'jest';
 import AgeCalculator from './../src/js/calculator.js';
 
 describe('AgeCalculator', () => {
@@ -14,30 +14,53 @@ describe('AgeCalculator', () => {
         const age = 30;
         const expectancy = 73;
         const newTest = new AgeCalculator(age, expectancy);
-        expect(newTest.mercuryCalculator()).toEqual(30);
+        expect(newTest.mercuryCalculator()).toEqual(7);
     });
 });
+
+
 describe('AgeCalculator', () => {
     test('should return the age in Venus years', () => {
         const age = 30;
         const expectancy = 73;
         const newTest = new AgeCalculator(age, expectancy);
-        expect(newTest.venusCalculator()).toEqual(30);
+        expect(newTest.venusCalculator()).toEqual(19);
     });
 });
+
 describe('AgeCalculator', () => {
     test('should return the age in Mars years', () => {
         const age = 30;
         const expectancy = 73;
         const newTest = new AgeCalculator(age, expectancy);
-        expect(newTest.earthCalculator()).toEqual(30);
+        expect(newTest.marsCalculator()).toEqual(56);
     });
 });
+
 describe('AgeCalculator', () => {
     test('should return the age in Jupiter years', () => {
         const age = 30;
         const expectancy = 73;
         const newTest = new AgeCalculator(age, expectancy);
-        expect(newTest.jupiterCalculator()).toEqual(30);
+        expect(newTest.jupiterCalculator()).toEqual(356);
+    });
+});
+
+
+describe('AgeCalculator', () => {
+    test('should return a message if your age equals average life expectancy', () => {
+        const age = 73;
+        const expectancy = 73;
+        const newTest = new AgeCalculator(age, expectancy);
+        expect(newTest.lifeCalculator()).toEqual('You are 73 years old and that is equal to the average life expectancy on Earth.');
+    });
+});
+
+describe('AgeCalculator', () => {
+    test('should return the number of years lived beyond average Earth life expectancy', () => {
+        const age = 93;
+        const expectancy = 73;
+        const newTest = new AgeCalculator(age, expectancy);
+        expect(newTest.lifeCalculator()).toEqual('You have lived 20 years beyond average life expectancy on Earth.');
     });
 });
